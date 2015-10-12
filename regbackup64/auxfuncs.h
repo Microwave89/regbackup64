27fa2274d1fb8cac3b22feea9177a6dbe56ee54d
Microwave89 <teslahv@gmail.com>
2015-10-12 03:25:42 +0200

    Added initial files

---
+++ regbackup64/auxfuncs.h
@@ -0,0 +1,5 @@
+#pragma once
+extern NTSTATUS syscallStub(ULONG syscallNum, ...);
+//__forceinline extern void myTerminate(void);
+extern BOOLEAN mymemcmp(char* src1, char* src2, SIZE_T length);
+extern SIZE_T mywcslen(WCHAR* pWcharString);
